741a9c6a31cd8bd84030eda5dca695f6b0c45031
N. Gasquet <ngasquet@hotmail.com>
2017-12-19 16:57:32 +0100

    Add project files.

---
+++ easyHangman/Scripts/indexController.js
@@ -0,0 +1,181 @@
+﻿var IndexController = (function () {
+    var letters =  {
+        letters: [
+            { letter: 'a' },
+            { letter: 'b' },
+            { letter: 'c' },
+            { letter: 'd' },
+            { letter: 'e' },
+            { letter: 'f' },
+            { letter: 'g' },
+            { letter: 'h' },
+            { letter: 'i' },
+            { letter: 'j' },
+            { letter: 'k' },
+            { letter: 'l' },
+            { letter: 'm' },
+            { letter: 'n' },
+            { letter: 'o' },
+            { letter: 'p' },
+            { letter: 'q' },
+            { letter: 'r' },
+            { letter: 's' },
+            { letter: 't' },
+            { letter: 'u' },
+            { letter: 'v' },
+            { letter: 'w' },
+            { letter: 'x' },
+            { letter: 'y' },
+            { letter: 'z' }
+        ]
+    };
+    var difficulty = "";
+    var word = "";
+    var tries = 100;
+    var audioLaughtEnd = null;
+    var audioBackground = null;
+
+    function IndexController() {
+        gameInitialize();
+        events();
+    }
+
+    function gameInitialize() {
+        audioBackground = playSong("/Assets/orage_tonnerre_04.mp3", true, true)
+    }
+
+    function randomWordsByDifficulty(difficulty) {
+        $.get("/data/words.json", function (data) {
+            var lengthDifficulty = data[difficulty].length - 1;
+            var rnd = Math.ceil(Math.random() * lengthDifficulty);
+            word = data[difficulty][rnd].mot;
+            renderWordInHTML(word);
+        })
+    }
+
+    function renderWordInHTML(word) {
+
+        var templateKey = $("#findLetterTemplate").html();
+        var render = Mustache.render(templateKey, null);
+
+        for (var i = 0; i < word.length; i++) {
+            setTimeout(function (cpt) {
+                $(".secret-word").append(render);
+            }, 300 * i, i)
+        }
+    }
+
+    function renderKeyboardHTML(word) {
+        var templateKey = $("#btnLetter").html();
+        var render = Mustache.render(templateKey, letters);
+        $(".keyboard div.row").html(render);
+    }
+
+    function events() {
+
+        $("div.step1").on("click", "button", function (evt) {
+            $(evt.delegateTarget).removeClass("fadeIn").addClass("fadeOut");
+            difficulty = $(this).val();
+
+            setTimeout(function () {
+                $(evt.delegateTarget).hide();
+                loadStep2();
+            }, 1000)
+        });
+
+        $("div.keyboard").on("click", "button", function (evt) {
+
+            var audio = playSong("/Assets/gargouilli-1.mp3", false, true);
+            findLetterInWord($(this).val());
+
+            $(audio).on("playing", onPlaying);
+            $(audio).on("ended", onEnded);
+        });
+
+        $("div.keyboard").on("mouseover", "button", function (evt) {
+            $(this).addClass("rubberBand");
+            setTimeout(() => {
+                $(this).removeClass("rubberBand");
+            }, 1000)
+        });
+
+        function onPlaying() {
+            $("div.keyboard button").prop("disabled", true);
+        }
+
+        function onEnded() {
+            $("div.keyboard button").prop("disabled", false)
+        }
+
+        $("#endGameModal").on("hide.bs.modal", function () {
+            audioLaughtEnd.pause();
+        });
+    }
+
+    function loadStep2() {
+        randomWordsByDifficulty(difficulty);
+        renderKeyboardHTML();
+    }
+
+    function findLetterInWord(letter) {
+        var lengthChars = (word.match(new RegExp(letter, "g")) || []).length;
+        console.log(lengthChars)
+
+        if (lengthChars > 0) {
+
+            for (var i = 0; i < word.length; i++) {
+                if (letter === word[i])
+                    $(".secret-word label:eq(" + i + ")").text(letter)
+            }
+        }
+        else {
+            errorLetter();
+        }
+
+    }
+
+    function errorLetter() {
+        if (tries > 0) {
+            tries -= 10;
+            $(".progress-bar").html(tries / 10).css("width", tries);
+        }
+
+        switch (true) {
+            case tries === 0:
+                endGame();
+                break;
+            case tries <= 40:
+                stressMode();
+                break;
+        }
+        
+    }
+
+    function endGame() {
+        audioLaughtEnd = playSong("/Assets/sf_rire_creature_04.mp3", true, true);
+        $("h1").addClass("hinge").css("color" , "#610B21");
+
+        $("#endGameModal").modal("show");
+    }
+
+    function playSong(path, loop, autoplay) {
+        var audio = new Audio(path);
+        audio.loop = loop;
+        audio.autoplay = autoplay;
+        return audio;
+    }
+
+    function stressMode() {
+        audioBackground.pause();
+        $("section").addClass("stress");
+        $(".keyboard").addClass("pulse");
+        playSong("/Assets/SF-coeur-echograph24.mp3", true, true);
+    }
+
+    return IndexController;
+})();
+
+
+$(document).ready(function () {
+    var indexCtrl = new IndexController();
+});
---
+++ easyHangman/readme.md
@@ -0,0 +1,24 @@
+1.Creer un nouveau projet (site web)
+2.Creer l'architecture de l'application
+3.Ajouter les scripts (Musatche, Jquery, bootstrap,...)
+4.Placer dans un dossier Data le fichier words.json
+5.Choisir une difficulté (Facile, Moyen, Difficile)
+6.Générer un mot alétoire (Math.random() * length de la difficulté)
+7.Créer un clavier en boutton html (26 bouttons)
+8.Afficher dans la console la lettre cliquée (value du button pour trouver la lettre)
+9.Effectuer le décompte des essais (10 essais)
+10.Afficher dans la console "Pendu" (quand il ne reste plus d'essais)
+11.Afficher le mot de l'étape 6 en HTML
+12.afficher la lettre dans le mot HTML (etape 11) si elle existe après le clique (Etape 8)
+13.afficher le nombre d'essais dans la page
+
+## Bonus
+Afficher dans une Modal l'etape 10
+Ajouter un bouton pour afficher l'indice et enlever 2 coups aux essais
+Ajouter des animations (Animate css) pour l'affichage des lettres trouvées
+Ajouter une modale de Victoire avec :
+        1 étoile (si reste a jouer > 4 et < 6)
+        2 étoiles (si reste a jouer >= 6 et < 10)
+        3 étoiles (si reste a jouer =10)
+
+
